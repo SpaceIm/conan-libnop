302dff5da380b52948bcddc5ca3846a46c106996
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-11 16:10:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# conan-libnop
+
+## Package Status
+
+| Windows | Linux | macOS |
+|:-------:|:-----:|:-----:|
+|[![Build status](https://ci.appveyor.com/api/projects/status/5dyn397tpbdf6ndf/branch/testing%2Fcci.20200728?svg=true)](https://ci.appveyor.com/project/SpaceIm/conan-libnop)|[![Build status](https://github.com/SpaceIm/conan-libnop/workflows/.github/workflows/conan.yml/badge.svg?branch=testing%2Fcci.20200728)](https://github.com/SpaceIm/conan-libnop/actions?query=branch%3Atesting%2Fcci.20200728)|[![Build Status](https://travis-ci.com/SpaceIm/conan-libnop.svg?branch=testing%2Fcci.20200728)](https://travis-ci.com/SpaceIm/conan-libnop)|
+
+## Description
+
+[Conan.io](https://conan.io) package for [libnop](https://github.com/google/libnop).
---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, tools
+import glob
+import os
+
+
+class LibnopConan(ConanFile):
+    name = "libnop"
+    description = "libnop is a header-only library for serializing and " \
+                  "deserializing C++ data types without external code " \
+                  "generators or runtime support libraries."
+    license = "Apache-2.0"
+    topics = ("conan", "libnop", "header-only", "serialization", "deserialization")
+    homepage = "https://github.com/google/libnop"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "compiler"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def configure(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 14)
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = glob.glob("libnop-*")[0]
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("*", dst="include", src=os.path.join(self._source_subfolder, "include"))
+
+    def package_info(self):
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+        self.cpp_info.bindirs = []
+        self.cpp_info.frameworkdirs = []
